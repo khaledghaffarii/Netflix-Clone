559fc48ae63c7eaeb6cf32a0f006e21962676372
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-01-15 22:02:37 +0100

    refactor: use isLargeRow to differentiate images width from original movies

--- src/Row.js
+++ src/Row.js
@@ -24,13 +24,13 @@ fetchData()
                 <div className="row__posters">
                    {/* row__poster */}
                    {
-                    movies.map(movie =>(
-                        <img 
-                        key={movie.id}
-                        className="row__poster"
-                        src={`${base_url}${movie.poster_path}`} 
-                        alt= {movie.name} />
-                       ))
+                        movies.map(movie =>(
+                            <img 
+                            key={movie.id}
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            src={`${base_url}${ isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                            alt= {movie.name} />
+                        ))
                    }
                 </div>
         </div>
