dd26953879350e99eed2f9a63bd2c5250acdbccb
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-01-15 17:16:20 +0100

    refactor: using useEffect for fetch all data movies befor'showing this in Row component

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
+import axios from './axios'
+const Row = ({title , fetchUrl}) => {
+    const [movies , SetMovies ] = useState([])
+ // IF [] RUN ONCE WHEN THE ROW LOADS, AND DON'T RUN AGAIN
+    useEffect( ()=> {
+   async function fetchData(){
+ const request= await axios.get(fetchUrl);
+// console.log(request)
+ SetMovies(request.data.results)
+ return request;
+}
+fetchData()
 
-const Row = () => {
+ },[fetchUrl]);
+ console.log(movies)
     return (
         <div>
-            
+            <h2> {title} </h2>
         </div>
     )
 }
