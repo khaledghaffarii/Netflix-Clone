05bb78ef79191dd33158ee30295b5fab0ea006e1
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-01-15 17:12:52 +0100

    refactor: fetched titles and subtitles by fetchRequest in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Row from './Row';
+import Requests from './Requests'
 function App() {
   return (
     <div className="App">
 
-        <Row title="NETFLIX ORIGNAL"/>
-        <Row title="Trending Now"/>  
+        <Row title="NETFLIX ORIGNAL" fetchUrl={Requests.fetchNetflixOriginals}/>
+        <Row title="Trending Now" fetchUrl={Requests.fetchTrending}/>  
 
     </div>
   );
