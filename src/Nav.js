182404046e6f1ea00149c3530d11c3d0aa1811ff
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-01-17 11:41:09 +0100

    refactor: replace smile Logo per button `Logout_Link` in Nav component

--- src/Nav.js
+++ src/Nav.js
@@ -22,12 +22,7 @@ const Nav = () => {
         alt="Netflix Logo"
         />
       </a>
-     
-      <img
-        className="nav_avatar"
-        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
-        alt="Netflix Logo"
-      />
+      <a href="#" className="Logout redButton" data-uia="header-login-link">Logout</a>
     </div>
     )
 }
