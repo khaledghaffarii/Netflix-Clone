bb62c1c55cb845a6c7e0390b291733aa4d8bf554
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-01-17 09:32:49 +0100

    refactor: add link in `Netflix Log` for nav home in Nav component

--- src/Nav.js
+++ src/Nav.js
@@ -15,11 +15,14 @@ const Nav = () => {
     }, [] );
     return (
     <div className={`nav ${show && "nav_black"}`}>
-      <img
+      <a href='/'>
+         <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
         alt="Netflix Logo"
-      />
+        />
+      </a>
+     
       <img
         className="nav_avatar"
         src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
